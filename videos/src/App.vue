c9cd7689c4ab0a64dda93a66c65dd4a89b2c4c31
Remigius Stalder <remigius.stalder@vanillaplan.ch>
2026-03-25 16:39:43 +0100

    initial commit

---
+++ videos/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { videos } from '@/videos.ts'
+
+</script>
+
+<template>
+  <div style="width: 100%; text-align: center">
+    <div v-for="video in videos" style="padding: 5px">
+      <iframe width="560" height="315" :src="video.src" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+  </div>
+</template>
---
+++ videos/src/videos.ts
@@ -0,0 +1,8 @@
+
+export const videos = [
+  { src: 'https://youtube.com/embed/2dYX-f7uPSo' }, // camp
+  { src: 'https://youtube.com/embed/48v65Sqieec' }, // WS
+  { src: 'https://youtube.com/embed/X-s977Z8CaY' }, // spin
+  { src: 'https://youtube.com/embed/tVDssK0bGz0' }, // X-Alps
+  { src: 'https://youtube.com/embed/voHBmvKsaZU' } // 125y
+]
