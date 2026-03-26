f9c2e451f6aed977501acee8a0e289f4096a09ee
Remigius Stalder <remigius.stalder@vanillaplan.ch>
2026-03-26 10:49:53 +0100

    add link to sgbf home page

--- videos/src/App.vue
+++ videos/src/App.vue
@@ -9,6 +9,7 @@ const videoHeight = Math.floor(videoWidth / 1.77777778)
 
 <template>
   <div style="width: 100%; text-align: center">
+    <p style="color: white; font-family: Roboto, Helvetica, Arial, sans-serif">Weitere Infos findest du hier: <a style="color: white" href="https://sgbaselfricktal.ch/fliegen-lernen/">Fliegen lernen</a></p>
     <div v-for="video in videos" style="padding: 5px">
       <iframe style="max-width: calc(100% - 10px)" :width="videoWidth" :height="videoHeight" :src="video.src" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
