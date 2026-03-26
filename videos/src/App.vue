ee19f1d002f6afa75799b7f72fa0a2c861e0c99c
Remigius Stalder <remigius.stalder@vanillaplan.ch>
2026-03-26 10:06:21 +0100

    adjust width of iframe to screen width

--- videos/src/App.vue
+++ videos/src/App.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 import { videos } from '@/videos.ts'
 
+const width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+const videoWidth = width > 570 ? 560 : width - 10
+const videoHeight = Math.floor(videoWidth / 1.77777778)
+
 </script>
 
 <template>
   <div style="width: 100%; text-align: center">
     <div v-for="video in videos" style="padding: 5px">
-      <iframe width="560" height="315" :src="video.src" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe style="max-width: calc(100% - 10px)" :width="videoWidth" :height="videoHeight" :src="video.src" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
   </div>
 </template>
